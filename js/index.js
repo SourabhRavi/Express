186398b70cb1a7ef0a41cc0744753075582b37ee
SourabhRavi <skravi0657@gmail.com>
2021-08-19 14:01:48 +0530

    update

--- js/index.js
+++ js/index.js
@@ -2,40 +2,42 @@ const searchForm = document.getElementById('searchForm')
 const searchString = document.getElementById('searchString')
 const resultsEl = document.getElementById('results')
 
-searchForm.addEventListener('submit', function(e) {
-    e.preventDefault()
-    const q = searchString.value.trim()
-    console.log(q)
-    search(q)
+searchForm.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const q = searchString.value.trim()
+  // console.log(q)
+  search(q)
 })
 
-searchString.addEventListener('input', function(e) {
-    const q = searchString.value.trim()
-    console.log(q)
-    search(q)
+searchString.addEventListener('input', function (e) {
+  const q = searchString.value.trim()
+  // console.log(q)
+  search(q);
+  if (searchString.value === "") {
+    trending();
+  }
 })
 
-// Search function //
 
 function search(q) {
-    const apikey = 'ftUwVbVYCio80g6YwUCyTUIyy0vs6QKP'
-    const path = 'https://api.giphy.com/v1/gifs/search?api_key=' + apikey + '&q=' + q + '&limit=50'
+  const apikey = 'ftUwVbVYCio80g6YwUCyTUIyy0vs6QKP'
+  const path = 'https://api.giphy.com/v1/gifs/search?api_key=' + apikey + '&q=' + q + '&limit=50'
 
-    const p = fetch(path)
-    p.then(function(res) {
-        return res.json()
-    }).then(function(json) {
-        console.log(json.data)
+  const p = fetch(path)
+  p.then(function (res) {
+    return res.json()
+  }).then(function (json) {
+    // console.log(json.data)
 
-        let resultsHTML = ''
+    let resultsHTML = ''
 
-        json.data.forEach(function(obj) {
-            console.log(obj.images.fixed_width_downsampled)
+    json.data.forEach(function (obj) {
+      // console.log(obj.images.fixed_width_downsampled)
 
-            const url = obj.images.fixed_width_downsampled.url
-            const width = obj.images.fixed_width_downsampled.width
-            const alt = obj.title
-            resultsHTML += `<div class="content">
+      const url = obj.images.fixed_width_downsampled.url
+      const width = obj.images.fixed_width_downsampled.width
+      const alt = obj.title
+      resultsHTML += `<div class="content">
             <a href="` + url + `" target="_blank">
               <div class="content-overlay"></div>
               <img class="content-image" src="` + url + `">
@@ -44,35 +46,34 @@ function search(q) {
               </div>
             </a>
           </div>`
-        })
-
-        resultsEl.innerHTML = resultsHTML
-    }).catch(function(err) {
-        console.log(err.message)
     })
+
+    resultsEl.innerHTML = resultsHTML
+  }).catch(function (err) {
+    console.log(err.message);
+  })
 }
 
 function trending() {
-    const apikey = 'ftUwVbVYCio80g6YwUCyTUIyy0vs6QKP'
-    const path = 'https://api.giphy.com/v1/gifs/trending?api_key=' + apikey
+  const apikey = 'ftUwVbVYCio80g6YwUCyTUIyy0vs6QKP'
+  const path = 'https://api.giphy.com/v1/gifs/trending?api_key=' + apikey
 
-    const p = fetch(path)
-    p.then(function(res) {
-        return res.json()
-    }).then(function(json) {
-        console.log(json.data)
+  const p = fetch(path)
+  p.then(function (res) {
+    return res.json()
+  }).then(function (json) {
+    // console.log(json.data)
 
-        let resultsHTML = ''
+    let resultsHTML = ''
 
-        json.data.forEach(function(obj) {
-            console.log(obj.images.fixed_width_downsampled)
-
-            const url = obj.images.fixed_width_downsampled.url
-            const width = obj.images.fixed_width_downsampled.width
-            const height = obj.images.fixed_width_downsampled.height
-            const usedHeight = (0.5 * height)
-            const alt = obj.title
-            resultsHTML += `<div class="content">
+    json.data.forEach(function (obj) {
+      // console.log(obj.images.fixed_width_downsampled);
+      const url = obj.images.fixed_width_downsampled.url;
+      const width = obj.images.fixed_width_downsampled.width;
+      const height = obj.images.fixed_width_downsampled.height;
+      const usedHeight = (0.5 * height);
+      const alt = obj.title;
+      resultsHTML += `<div class="content">
             <a href="` + url + `" target="_blank">
               <div class="content-overlay" style="height=` + usedHeight + `px;"></div>
               <img class="content-image" src="` + url + `">   
@@ -81,14 +82,14 @@ function trending() {
                 <h3 class="content-title">` + alt + `</h3>
               </div>
             </a>
-          </div>`
-        })
+          </div>`;
+    });
 
-        resultsEl.innerHTML = resultsHTML
-    }).catch(function(err) {
-        console.log(err.message)
-    })
+    resultsEl.innerHTML = resultsHTML
+  }).catch(function (err) {
+    console.log(err.message);
+  });
 }
 
 // Random GIFs //
-trending()
+trending();
